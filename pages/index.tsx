04d4e20a94bcb5bf37cbd5d7640a7c1932b66e75
Kyle <Kyle.fire@gmail.com>
2022-05-29 12:03:57 +0900

    lzgit - 2022-05-29T03:03:57.355Z

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       </Head>
 
       <div className='grid w-screen h-screen grid-cols-12 grid-rows-6 gap-4 bg-black'>
-        <div className='col-span-5 col-start-1 row-start-1 row-end-3 mt-10 ml-6 text-blue-500 border-2 border-blue-500 rounded-md'>
+        <div className='col-span-5 col-start-1 row-span-4 row-start-1 mt-10 ml-6 text-blue-500 border-2 border-blue-500 rounded-md'>
           <div className='px-1 py-2 ml-6 text-2xl text-center -translate-y-6 bg-black max-w-fit'>
             AlsoPeters-Info
           </div>
@@ -18,7 +18,7 @@ export default function Home() {
         <div className='col-span-7 row-span-5 mt-10 mr-6 border-2 border-red-500 rounded-md'>
           <p className='p-2 text-gray-200'>
             {' '}
-            My name is Kyle Ryan Garcia. I'm a web developer based out of Japan.
+            My name is Kyle Ryan Garcia. I'm a web developer living in Japan.
           </p>
         </div>
         <div className='col-start-6 col-end-13 row-start-6 row-end-6 mt-16 mb-6 mr-6 border-2 border-blue-500 rounded-md'>
