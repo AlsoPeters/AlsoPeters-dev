c84f61c2e72adebed0375e14b10f904ba58de69b
Kyle <Kyle.fire@gmail.com>
2022-05-30 18:22:17 +0900

    changed layout and onclick for categories

---
+++ components/AlsoPeters.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AlsoPeters() {
+  return (
+    <div>
+      <div className='px-1 py-2 ml-6 text-2xl text-center -translate-y-6 bg-black hover:text-purple-500 max-w-fit'>
+        AlsoPeters
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,95 @@
 import Head from 'next/head';
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [alsoPetersSelected, setAlsoPetersSelected] = useState(true);
+  const [resumeSelected, setResumeSelected] = useState(false);
+  const [githubSelected, setGithubSelected] = useState(false);
+
+  function selectResume() {
+    setAlsoPetersSelected(false);
+    setResumeSelected(true);
+    setGithubSelected(false);
+  }
+
+  function selectGithub() {
+    setAlsoPetersSelected(false);
+    setResumeSelected(false);
+    setGithubSelected(true);
+  }
+
+  function selectAlsoPeters() {
+    setAlsoPetersSelected(true);
+    setResumeSelected(false);
+    setGithubSelected(false);
+  }
+
   return (
     <>
       <Head>
         <title>AlsoPeters.dev</title>
       </Head>
 
-      <div className='grid w-screen h-screen grid-cols-12 grid-rows-6 gap-4 bg-black'>
-        <div className='col-span-5 col-start-1 row-span-4 row-start-1 mt-10 ml-6 text-blue-500 border-2 border-blue-500 rounded-md'>
-          <div className='px-1 py-2 ml-6 text-2xl text-center -translate-y-6 bg-black max-w-fit'>
-            AlsoPeters-Info
+      <div className='flex flex-row w-screen h-screen px-4 pt-10 pb-4 bg-black'>
+        <div className='flex flex-col w-1/3 mr-2'>
+          <div
+            className={
+              alsoPetersSelected === true
+                ? 'mb-1 border-2 text-purple-500 border-purple-500 rounded-md grow'
+                : 'mb-1 border-2 text-white border-white rounded-md flex-none'
+            }
+          >
+            <div
+              onClick={selectAlsoPeters}
+              className='px-1 py-2 ml-6 text-2xl text-center -translate-y-6 bg-black select-none hover:cursor-pointer max-w-fit'
+            >
+              AlsoPeters
+            </div>
+          </div>
+
+          <div
+            className={
+              resumeSelected === true
+                ? 'mb-1 border-2 text-purple-500 border-purple-500 rounded-md grow mt-4'
+                : 'mb-1 border-2 h-16 text-white border-white rounded-md flex-none mt-4'
+            }
+          >
+            <div
+              onClick={selectResume}
+              className='px-1 ml-6 text-2xl text-center -translate-y-4 bg-black select-none max-w-fit hover:cursor-pointer '
+            >
+              Resume
+            </div>
+          </div>
+
+          <div
+            className={
+              githubSelected === true
+                ? 'mb-1 border-2 text-purple-500 border-purple-500 rounded-md grow mt-4'
+                : 'mb-1 border-2 h-16 text-white border-white rounded-md flex-none mt-4'
+            }
+          >
+            <div
+              onClick={selectGithub}
+              className='px-1 ml-6 text-2xl text-center -translate-y-4 bg-black select-none max-w-fit hover:cursor-pointer '
+            >
+              Github
+            </div>
           </div>
         </div>
-        <div className='col-span-5 row-span-1 row-start-6 mt-16 mb-6 ml-6 border-2 border-green-300 rounded-md '></div>
 
-        <div className='col-span-7 row-span-5 mt-10 mr-6 border-2 border-red-500 rounded-md'>
-          <p className='p-2 text-gray-200'>
-            {' '}
-            My name is Kyle Ryan Garcia. I'm a web developer living in Japan.
-          </p>
-        </div>
-        <div className='col-start-6 col-end-13 row-start-6 row-end-6 mt-16 mb-6 mr-6 border-2 border-blue-500 rounded-md'>
-          <div className='px-1 ml-4 font-bold text-blue-500 -translate-y-3 bg-black max-w-fit'>
-            Currently Listening To
+        <div className='flex flex-col w-2/3 ml-2 '>
+          <div className='border-2 border-white rounded-md grow'>
+            <p className='p-4 text-gray-200'>
+              {' '}
+              My name is Kyle Ryan Garcia and I'm a web developer living in
+              Japan.
+            </p>
+          </div>
+          <div className='flex-none h-16 mt-4 border-2 rounded-md border-grey-500'>
+            <div className='px-1 ml-4 font-bold text-blue-500 -translate-y-3 bg-black max-w-fit'>
+              Currently Listening To
+            </div>
           </div>
         </div>
       </div>
